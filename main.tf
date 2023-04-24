7cc76fce61bbdcea805d0eb1619ab60b56976dbe
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-04-24 12:23:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_lb" "ALB" {
   internal           = false
   load_balancer_type = "application"
   subnets            = ["subnet-0b7417c037d3b736f", "subnet-08379ffadd1a6be7e"]
-  security_groups    = ["sg-12345678"]
+  security_groups    = ["sg-094ac35818b33b834"]
 }
 
 resource "aws_lb_target_group" "ALBTG" {
