5a051ff2cc9100595028f8e33454cc9dc2ea48e6
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-04-24 12:13:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,14 +19,3 @@ resource "aws_lb_target_group" "ALBTG" {
     path = "/"
   }
 }
-
-resource "aws_lb_listener" "ALBLSNR" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.example.arn
-  }
-}
