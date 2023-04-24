52716251c028b07a3a9e8228b1ec291e8b487394
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-04-24 15:12:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,9 @@ resource "aws_lb_listener" "ALBLSNR" {
     target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/ALB-tg20230424064929632600000001/2069cce7616e00e0"
   }
 }
+
+resource "aws_lb_target_group_attachment" "web_ALB" {
+  target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/ALB-tg20230424064929632600000001/2069cce7616e00e0"
+  target_id        = "i-0b7120b20f0e8990f".id
+  port             = 80
+}
