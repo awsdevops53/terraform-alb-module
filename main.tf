c0fc1e18cd3d5183442678bde8a3c70eac02ab1a
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-04-25 10:37:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,6 @@ resource "aws_lb_listener" "ALBLSNR" {
 
 resource "aws_lb_target_group_attachment" "webserver-alb" {
   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/ALB-tg20230424064929632600000001/2069cce7616e00e0"
-  target_id        = aws_instance.webserver-tg
+  target_id        = aws_instance.webserver_tg
   port             = 80
 }
