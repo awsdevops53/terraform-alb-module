88bcea0b5c0b879a491ed55e7b559a93072807b3
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-04-25 11:55:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ resource "aws_lb_listener" "ALBLSNR" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "svc" {
+resource "aws_lb_target_group_attachment" "ALBTG" {
   target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/ALB-tg20230424064929632600000001/2069cce7616e00e0"
-  target_id        = "${aws_instance.svc.id}"
+  target_id        = "${aws_instance.ALBTG.id}"
   port             = 80
 }
