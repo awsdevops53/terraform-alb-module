f435eddf9cce7cbc4e02d5b5a3c07ece3b3b53a1
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-04-26 18:00:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,12 +25,12 @@ resource "aws_lb_listener" "ALBLSNR" {
 
   default_action {
     type             = "forward"
-    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/tf-example-lb-alb-tg/d0f9bbe3ab5a0a56"
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/tf-example-lb-alb-tg/73bcbee3c1abc7c4"
   }
 }
 
 resource "aws_lb_target_group_attachment" "tg_attachment_test1" {
-    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/tf-example-lb-alb-tg/d0f9bbe3ab5a0a56"
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/tf-example-lb-alb-tg/73bcbee3c1abc7c4"
     target_id        = "i-0b7120b20f0e8990f, i-0b73c6d866fe1dd5a" 
     port             = 80
 }
