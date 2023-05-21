1c12fd6654f85a26d0c2396d857b508ab3058f0e
awsdevops53 <90223102+awsdevops53@users.noreply.github.com>
2023-05-21 20:31:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,18 +21,18 @@ resource "aws_lb_target_group" "targetgroup1"  {
  }
 
 resource "aws_lb_listener" "ALB-LSNR" {
-  load_balancer_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:loadbalancer/app/ALB-lb/d820158ed819f7cd"
+  load_balancer_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:loadbalancer/app/alb-rana/39231981d7f156d9"
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/alb-target-group/f4d6c2d18b77662f"
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/alb-tg/c825aaad54d10fdf"
   }
 }
 
 resource "aws_lb_target_group_attachment" "tg_attachment_test1" {
-    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/alb-target-group/f4d6c2d18b77662f"
+    target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:238393102293:targetgroup/alb-tg/c825aaad54d10fdf"
     target_id        = "i-000852422c66c4782, i-0c71a23a8eb7b528a" 
     port             = 80
 }
